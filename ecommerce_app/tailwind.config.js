eac01c46b127fb1c55d745f7429f67b236a2617b
abhijeet <chougaleabhii@gmail.com>
2024-05-13 04:39:46 +0530

    setup new project Ecommerce

---
+++ ecommerce_app/tailwind.config.js
@@ -0,0 +1,24 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter","sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+});
+
